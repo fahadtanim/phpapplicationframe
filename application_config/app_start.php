565ffb7b664f7f03ef6d6da19dcf0957bf714f20
Md. Fahad-Uz-Zaman Khan <fahad.khan@dsinnovators.com>
2019-04-11 12:16:58 +0600

    work on router.

--- application_config/app_start.php
+++ application_config/app_start.php
@@ -3,6 +3,7 @@
     require_once(APPLICATION_CONFIG."path_config.php");
     function app_start(){
         $route = new Router();
+        $route->map('','Task');
         $route->map('api/tasks/[:id]/[:num]','Task','edit');
         $route->map('api/task','Task');
         $route->map('**','Error');
--- application_config/router_config.php
+++ application_config/router_config.php
@@ -13,8 +13,14 @@
         }
 
         public function map($routeUrl, $controller_name, $action_name=''){
-            
             if($routeUrl == '**'){
+                $this->route_map[$routeUrl] = array(
+                    'controller' => $controller_name,
+                    'action' => $action_name
+                );
+            }
+            elseif (empty($routeUrl)) {
+                $routeUrl = 'index';
                 $this->route_map[$routeUrl] = array(
                     'controller' => $controller_name,
                     'action' => $action_name
@@ -35,39 +41,67 @@
         }
 
         public function route_start(){
+            echo "URL: ".$this->url."<br>";
             $controller_found = false;
-            foreach( $this->route_map as $route_key => $route_values ){
-                if(preg_match($route_key, $this->url)){
-                    preg_match($route_key, $this->url, $params);
-                    unset($params[0]);
-                    $params = array_values($params);
-                    $controller_name = $route_values['controller'].'Controller';
-                    $action = $route_values['action'];
-                    if( file_exists(CONTROLLER_PATH.$controller_name.'.php') ){
-                        require_once(CONTROLLER_PATH.$controller_name.'.php');
-                        if( class_exists($controller_name) ){
-                            $controller = new $controller_name();
-                            if(empty($action)){
-                                $action = 'index';
-                                $controller->$action();
-                            }
-                            elseif(method_exists($controller, $action)){
-                                if(count($params) > 0){
-                                    call_user_func_array(array($controller, $action), $params);
-                                }
-                                else{
-                                    $controller->$action();
-                                }
-                            }
-                            else{
-                                echo "action couldn't be found";
-                            }
+            if ($this->url == 'index') {
+                $controller_name = $this->route_map['index']['controller'].'Controller';
+                $action = $this->route_map['index']['action'];
+                if( file_exists(CONTROLLER_PATH.$controller_name.'.php') ){
+                    require_once(CONTROLLER_PATH.$controller_name.'.php');
+                    if( class_exists($controller_name) ){
+                        $controller = new $controller_name();
+                        if(empty($action)){
+                            $action = 'index';
+                            $controller->$action();
+                        }
+                        elseif(method_exists($controller, $action)){
+                            $controller->$action();
+                        }
+                        else{
+                            echo "action couldn't be found";
                         }
                     }
-                    $controller_found = true;
-                    break;
+                }
+                $controller_found = true;
+            }
+            else {
+                foreach( $this->route_map as $route_key => $route_values ){
+                    if ($route_key == 'index') {
+                        continue;
+                    }
+                    elseif(preg_match($route_key, $this->url)){
+                        preg_match($route_key, $this->url, $params);
+                        unset($params[0]);
+                        $params = array_values($params);
+                        $controller_name = $route_values['controller'].'Controller';
+                        $action = $route_values['action'];
+                        if( file_exists(CONTROLLER_PATH.$controller_name.'.php') ){
+                            require_once(CONTROLLER_PATH.$controller_name.'.php');
+                            if( class_exists($controller_name) ){
+                                $controller = new $controller_name();
+                                if(empty($action)){
+                                    $action = 'index';
+                                    $controller->$action();
+                                }
+                                elseif(method_exists($controller, $action)){
+                                    if(count($params) > 0){
+                                        call_user_func_array(array($controller, $action), $params);
+                                    }
+                                    else{
+                                        $controller->$action();
+                                    }
+                                }
+                                else{
+                                    echo "action couldn't be found";
+                                }
+                            }
+                        }
+                        $controller_found = true;
+                        break;
+                    }
                 }
             }
+            
 
             if($controller_found == false){
                 $controller_name = $this->route_map['**']['controller'].'Controller';
